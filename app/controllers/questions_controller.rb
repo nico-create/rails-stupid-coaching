2e57a1cb7658ad64fc2bbb758445e1b2d68ef9f1
Nico L <l.nico08@laposte.net>
2022-01-04 22:37:44 +0900

    basic app and basic scss

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:You]
+
+    if @question.downcase == 'i am going to work right now!'
+      @coach_answer = 'Great!'
+    elsif @question.end_with?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'questions#ask'
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
